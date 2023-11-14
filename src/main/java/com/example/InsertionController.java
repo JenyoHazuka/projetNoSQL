a65b164230f9738178cc826b14ed5145d9a0559a
JenyoHazuka <tony.dary10@gmail.com>
2023-11-14 19:30:42 +0100

    v1 Insert
    Test de la méthode insertion.
    Non fonctionnel pour l'instant.

---
+++ src/main/java/com/example/InsertionController.java
@@ -0,0 +1,48 @@
+package main.java.com.example;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
+import org.bson.Document;
+
+@Controller
+@RequestMapping("/inserer")
+public class InsertionController {
+
+    @Autowired
+    private InsertionArticle insertionArticle;
+
+    @GetMapping("/insertPage")
+    public String afficherPageInsertion() {
+        return "insertion";
+    }
+
+    @PostMapping("/inserer")
+    public String insererDonnee(@RequestParam String Namebar, @RequestParam int PriceUniquebar) {
+        insertionArticle.insererArticle(Namebar, PriceUniquebar);
+        return "redirect:/";
+    }
+
+    @Service
+    public static class InsertionArticle {
+        public void insererArticle(String nom, int prix) {
+            String connectionString = "mongodb://localhost:27017";
+            try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+                MongoDatabase database = mongoClient.getDatabase("ProjetNOsql");
+                MongoCollection<Document> collection = database.getCollection("ListeArticle");
+
+                Document document = new Document("nom", nom)
+                        .append("prix", prix);
+
+                collection.insertOne(document);
+
+                System.out.println("Article inséré : Nom = " + nom + ", Prix = " + prix);
+            }
+        }
+    }
+}
--- src/main/java/com/example/MainApp.java
+++ src/main/java/com/example/MainApp.java
@@ -1,3 +1,4 @@
+package main.java.com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
